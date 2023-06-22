9bd1541972543f10aab0f1d995399da927a4ea9d
ironshadowz <chenwl911@gmail.com>
2023-06-22 14:05:49 +0800

    'initial'

---
+++ src/main/java/sg/edu/nus/iss/PAFTut3/PafTut3Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.PAFTut3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafTut3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafTut3Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/PAFTut3/PafTut3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.PAFTut3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafTut3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
